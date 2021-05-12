ef60a4debabaaf5e5b96d12cf48e7afcc8a183f7
septst <parthibansekar87@gmail.com>
2021-05-12 13:37:58 +0530

    moved data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -32,14 +32,58 @@ const homeList = (req, res) => {
 };
 
 // Get 'Location Info' page
-const locationInfo = (req, res) => {
-    res.render("location-info", {title: "Location Info"});
-};
+const locationInfo = function(req, res){
+    res.render('location-info', {
+      title: 'Starcups',
+      pageHeader: {title: 'Starcups'},
+      sidebar: {
+        context: 'is on Loc8r because it has accessible wifi and space to sit '+
+        'down with your laptop and get some work done.',
+        callToAction: 'If you\'ve been and you like it - or if you don\'t -'+
+        'please leave a review to help other people just like you.'
+      },
+      location: {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        coords: {lat: 51.455041, lng: -0.9690884},           
+        openingTimes: [{                                     
+          days: 'Monday - Friday',
+          opening: '7:00am',
+          closing: '7:00pm',
+          closed: false
+        },{
+          days: 'Saturday',
+          opening: '8:00am',
+          closing: '5:00pm',
+          closed: false
+        },{
+          days: 'Sunday',
+          closed: true
+        }],
+        reviews: [{                                          
+          author: 'Parthiban Sekar',
+          rating: 5,
+          timestamp: '16 July 2013',
+          reviewText: 'What a great place. I can\'t say enough good things about it.'
+        },{
+          author: 'Charlie Chaplin',
+          rating: 3,
+          timestamp: '16 June 2013',
+          reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+        }]
+      }
+    });
+  };
 
 // Get 'Add Review' page
-const addReview = (req, res) => {
-    res.render("location-review-form", {title: "Add Review"});
-};
+const addReview = function(req, res){
+    res.render('location-review-form', {
+      title: 'Review Starcups on Loc8r',
+      pageHeader: { title: 'Review Starcups' }
+    });
+  };
 
 module.exports = {
     homeList,
