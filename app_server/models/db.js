841440476503f6a5f406209c74182b0d10c2956c
septst <parthibansekar87@gmail.com>
2021-05-13 16:44:15 +0530

    setup mongoose

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,30 +1,80 @@
 const mongoose = require('mongoose');
+const readline = require('readline');
 
-const dbUri = 'mongodb://localhost/loc8r';
-mongoose.connect(dbUri, {useUnifiedTopology: true, useNewUrlParser:true});
+const mainDbUri = 'mongodb://localhost/loc8r';
+const logDbUri = 'mongodb://localhost/loc8r-log';
 
-mongoose.connection.on('connected', () => {
-    console.log(`Mongoose connected to ${dbUri}`);
-});
+const connectOptions = {
+    useUnifiedTopology: true,
+    useNewUrlParser:true
+};
 
-mongoose.connection.on('error', err => {
-    console.log('Mongoose connection error:', err);
-});
+const connMain = mongoose.createConnection(mainDbUri, connectOptions);
+const connLog = mongoose.createConnection(logDbUri, connectOptions);
+const connections = [
+    connMain,
+    connLog
+];
 
-mongoose.connection.on('disconnected', () => {
-    console.log(`Mongoose disconnected from ${dbUri}`);
-});
+for (let connection of connections)
+{
+    let dbName = connection.name;
+    connection.on('connected', () => {
+        console.log(`Mongoose connected to ${dbName}`);
+    }); 
 
-const gracefulShutdown = (msg, callback) => {
-    mongoose.connection.close( () => {
-        console.log(`Mongoose disconnected through ${msg}`);
-        callback();
+    connection.on('error', err => {
+        console.log('Mongoose connection error in ${dbName}:', err);
+    }); 
+
+    connection.on('disconnected', () => {
+        console.log(`Mongoose disconnected from ${dbName}`);
+    });
+};
+
+//for windows
+if(process.platform === 'win32'){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on ('SIGINT', () => {
+        process.emit ("SIGINT");
+    });
+
+    rl.on ('SIGUSR2', () => {
+        process.emit ("SIGUSR2");
     });
 }
+
+const gracefulShutdown = (msg, callback) => {
+    for (let connection of connections)
+    {
+        connection.close( () => {
+            console.log(`Mongoose disconnected through ${msg}`);
+            callback();
+        });
+    };
+};
 
 // For nodemon starts
 process.once('SIGUSR2', () => {
     gracefulShutdown('nodemon restart', () => {
         process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// For app termination
+process.on('SIGINT', () => {                           
+    gracefulShutdown('app termination', () => {          
+      process.exit(0);                                   
+    });
+});
+
+// For Heroku app termination  
+process.on('SIGTERM', () => {                          
+    gracefulShutdown('Heroku app shutdown', () => {      
+      process.exit(0);                                   
     });
 });
