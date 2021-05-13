ed80a9e64ba96ad2f81dc8ac120e7e7faa913b63
septst <parthibansekar87@gmail.com>
2021-05-13 17:25:29 +0530

    removed setup mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dom = require('jsdom-global')();
 const favicon = require('favicons');
-require('./app_server/models/db');
+// require('./app_server/models/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
