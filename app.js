92b181e7b2300bf3916ea925a564baaea53c422a
septst <parthibansekar87@gmail.com>
2021-05-22 15:14:18 +0530

    Added allowed cross origins for localhosts

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(passport.initialize());
 
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
