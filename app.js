526be3de297f7d347f654f7e854ecef8933652ad
septst <parthibansekar87@gmail.com>
2021-05-22 15:29:06 +0530

    set multiple origins hack

--- app.js
+++ app.js
@@ -42,9 +42,12 @@ app.use(passport.initialize());
 // app.use(cors());
 
 app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  const allowedOrigins = ['http://localhost:4200', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
