249ecc4057a2806261933227f804e8e7d5bc9ddc
septst <parthibansekar87@gmail.com>
2021-05-26 23:49:03 +0530

    Update login.component.ts

--- app_public/src/app/login/login.component.ts
+++ app_public/src/app/login/login.component.ts
@@ -49,7 +49,7 @@ export class LoginComponent implements OnInit {
       .then(() => this.router.navigateByUrl("/"))
       .catch((err) => {       
         this.loginForm.enable();
-        this.formErrors = err.message;
+        this.formErrors = err;
       });
   }
 
